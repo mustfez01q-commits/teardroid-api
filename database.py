6cec51574dd3e3deb20058c6b88306cb43c6f606
mustfez01q-commits <mustfez01q@gmail.com>
2026-04-01 13:31:44 +0530

    database.py

--- database.py
+++ database.py
@@ -1,24 +1,23 @@
-from deta import Deta
-from os import getenv
+import motor.motor_asyncio
+import os
 
-deta = Deta(getenv("DETA_PROJECT_KEY"))
+# Teri asali MongoDB link yahan hai
+MONGO_URL = "mongodb+srv://a31692342_db_user:Arhambhai123@cluster0.cyrntn3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+db = client["teardroid_database"]
 
 def client_db():
-    return deta.Base("client")
-
+    return db["clients"]
 
 def notification_db():
-    return deta.Base("notification")
-
+    return db["notifications"]
 
 def command_db():
-    return deta.Base("command")
-
+    return db["commands"]
 
 def auth_db():
-    return deta.Base("auth")
-
+    return db["auth"]
 
 async def tear_drive():
-    return deta.Drive("teardroid")
+    return db["drive"]
